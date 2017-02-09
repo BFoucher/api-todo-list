96cecf34935db5235a17a7cb143475920abcdaa4
baptiste <baptiste@troopers.email>
2017-02-09 14:06:03 +0100

    :sparkles: base router

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const apiTodos = require('./api/todos');
 
 let app = express();
 let port = 3000;
 
-app.listen(port, () => {
-    console.log('Server start.')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extend: true}));
+
+//Api Todos Lits Router
+app.use('/api/todos', apiTodos);
+
+//Global routes
+app.get('/', (req, res) => {
+    res.json({message: 'hello world'});
 });
 
-app.get('/', (req, res) => {
-    res.send({message: 'hello world'});
+app.get('/*', (req, res) => {
+    res.sendStatus(404);
+});
+
+
+
+app.listen(port, () => {
+    console.log('Server start.')
 });
